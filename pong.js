f4371906b8d91c62e2bba5247a7758f7b5810337
devGuiMattos <guidemattosmarques@gmail.com>
2026-03-15 01:19:12 -0300

    Update pong.js

--- pong.js
+++ pong.js
@@ -11,6 +11,12 @@ let radius = diameter / 2;
 let xPlayer = 10;
 let yPlayer = 200;
 let lenPlayer = 90;
+let pPoints = 0;
+
+let xEnemy = 580;
+let yEnemy = 145;
+let lenEnemy = lenPlayer;
+let ePoints = 0;
 
 function setup() {
   createCanvas(600, 400);
@@ -20,6 +26,36 @@ function draw() {
   background(0);
   Ball();
   Player();
+  Enemy();
+  VerifyCollision();
+  Points();
+}
+
+function Points(){
+  if(xBall + radius > width){
+    pPoints += 1;
+  } else if(xBall - radius < 0){
+    ePoints += 1;
+  }
+  text(pPoints, 250, 50);
+  text(ePoints, 350, 50);
+  fill(255)
+  textSize(32);
+}
+function VerifyCollision() {
+  if (xBall - radius < xPlayer + 7 && 
+      yBall > yPlayer && 
+      yBall < yPlayer + lenPlayer) {
+    xBallSpeed *= -1;
+    xBall = xPlayer + 7 + radius;
+  }
+
+  if (xBall + radius > xEnemy && 
+      yBall > yEnemy && 
+      yBall < yEnemy + lenEnemy) {
+    xBallSpeed *= -1;
+    xBall = xEnemy - radius;
+  }
 }
 
 function Ball(){
@@ -39,15 +75,33 @@ function Player(){
   rect(xPlayer, yPlayer, 7, lenPlayer)
   
   if(keyIsDown(UP_ARROW)){
-    yPlayer -= 10;
+    yPlayer -= 4;
   }
   if(keyIsDown(DOWN_ARROW)){
-    yPlayer += 10
+    yPlayer += 4;
   }
   
   if(yPlayer + lenPlayer > height){
-    yPlayer = ;
+    yPlayer = height - lenPlayer;
   } else if(yPlayer < 0){
     yPlayer = 0;
+  }
+}
+
+function Enemy(){
+  rect(xEnemy, yEnemy, 7, lenEnemy)
+  let targetY = yBall - lenEnemy / 2;
+  let enemySpeed = 4;
+  
+  if(yEnemy + lenEnemy > height){
+    yEnemy = height - lenEnemy;
+  } else if(yEnemy < 0){
+    yEnemy = 0;
+  }
+  
+  if (yEnemy < targetY) {
+    yEnemy += enemySpeed;
+  } else {
+    yEnemy -= enemySpeed;
   }
 }
